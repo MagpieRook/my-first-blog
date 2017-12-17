5fb3e78296629aca89d073a332953b3a86722694
Alex Rook <alex@alexrook.com>
2017-12-17 14:47:32 -0600

    Modified templates to display posts from database.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
+from django.utils import timezone
 from .models import Post
 
-admin.site.register(Post)
+def make_published(modeladmin, request, queryset):
+    queryset.update(published_date=timezone.now())
+make_published.short_description = "Publish selected posts"
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['author', 'title', 'created_date', 'published_date']
+    ordering = ['author', 'title', 'created_date', 'published_date']
+    actions = [make_published]
+
+admin.site.register(Post, PostAdmin)
