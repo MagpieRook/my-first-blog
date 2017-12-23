9dddf69f9620f8dd4641ea3296e9cc7e985614cb
Alex Rook <alex@alexrook.com>
2017-12-22 21:28:30 -0600

    Final djangogirls tutorial, including PostgreSQL

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils import timezone
-from .models import Post
+from .models import Post, Comment
 
 def make_published(modeladmin, request, queryset):
     queryset.update(published_date=timezone.now())
@@ -8,7 +8,13 @@ make_published.short_description = "Publish selected posts"
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ['author', 'title', 'created_date', 'published_date']
-    ordering = ['author', 'title', 'created_date', 'published_date']
+    ordering = ['published_date', 'created_date']
     actions = [make_published]
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['post', 'author', 'created_date', 'approved_comment']
+    ordering = ['created_date', 'approved_comment']
+    actions = []
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
